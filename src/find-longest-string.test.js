1664591ae726beb52fac3bb290fa4c5b2bb17cfc
Jennifer Qiao <qiaojennifer97@gmail.com>
2022-07-08 11:01:28 +0100

    add longest test

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,7 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["jennifer", "apple123", "conorren"])).toBe(
+    "jennifer"
+  );
 });
